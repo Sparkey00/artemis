af9ff1e87a1bc6d5b65427ca6831e915426d216e
moiseev <moiseev@rozetka.com.ua>
2022-04-18 19:23:08 +0300

    added axios and .env

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,21 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 loadFonts()
+
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+
+// Important: If axios is used with multiple domains, the AUTH_TOKEN will be sent to all of them.
+// See below for an example using Custom instance defaults instead.
+axios.defaults.headers.common['Authorization'] = 'a';
+
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 createApp(App)
   .use(router)
   .use(store)
+  .use(VueAxios, axios)
   .use(vuetify)
   .mount('#app')
